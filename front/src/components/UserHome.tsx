e1f26e12bbbc55d7d7830b28aa2fc67a1eb6f05f
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-04-28 13:35:15 +0900

    AppBarのレイアウトを修正した。

--- front/src/components/UserHome.tsx
+++ front/src/components/UserHome.tsx
@@ -20,7 +20,7 @@ export const UserHome = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             電気代集金システム（支払い者用）
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">ゲスト</Button>
         </Toolbar>
       </AppBar>
         </div>
