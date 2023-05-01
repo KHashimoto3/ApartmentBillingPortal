c96008de65db363f0f75da9d29285069adf057c8
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-01 17:51:59 +0900

    ページのタイトルを変更した。

--- front/src/components/user/contentPage/Billing.tsx
+++ front/src/components/user/contentPage/Billing.tsx
@@ -3,7 +3,7 @@ import "./Billing.scss";
 export const NextBilling = () => {
     return (
         <div className="homeContentsArea">
-            <h1>NextBilling</h1>
+            <h1>今月の請求</h1>
         </div>
     )
 }
