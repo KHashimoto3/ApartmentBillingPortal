535586b29f4df13d948ab2ec9f9dfe1c6312455a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-04 10:33:43 +0900

    支払額を表示するエリアを設置した。

--- front/src/components/user/contentPage/Billing.tsx
+++ front/src/components/user/contentPage/Billing.tsx
@@ -1,22 +1,23 @@
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 
 import "./Billing.scss";
 
 export const NextBilling = () => {
+
+    const paperStyle = {
+        height: '100px',
+    };
+
     return (
         <div className="homeContentsArea">
             <h1>今月の請求</h1>
             <Paper elevation={3} children={
                 <div className="billingCardArea">
-                    <Grid container spacing={1}>
-                        <Grid item xs={4}>
-                            <h3>合計：</h3>
-                        </Grid>
-                        <Grid item xs={8}>
-                            <h2>15,000円</h2>
-                        </Grid>
-                    </Grid>
+                    <h1>請求合計額：    3050円</h1>
+                    <hr />
+                    <p>今月請求額：     300円</p>
+                    <p>今月請求額：     300円</p>
+                    <p>今月請求額：     300円</p>
                 </div>
             } />
         </div>
