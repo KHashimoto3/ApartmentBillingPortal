00c6a52622a21fea575581d901aca3c28e8b00b3
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-05-04 11:03:05 +0900

    請求の項目を修正した。

--- front/src/components/user/contentPage/Billing.tsx
+++ front/src/components/user/contentPage/Billing.tsx
@@ -10,11 +10,11 @@ export const NextBilling = () => {
             <h1>今月の請求</h1>
             <Paper elevation={3} children={
                 <div className="billingCardArea">
-                    <h1>請求合計額：    3050円</h1>
+                    <h1>合計請求額：    3050円</h1>
                     <hr />
-                    <p>今月請求額：     300円</p>
-                    <p>今月請求額：     300円</p>
-                    <p>今月請求額：     300円</p>
+                    <p>使用量   ：     45</p>
+                    <p>今月請求額：     2160円</p>
+                    <p>繰越請求額：     7870円</p>
                 </div>
             } />
         </div>
