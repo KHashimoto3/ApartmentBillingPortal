6561eaefba6f33d1aa1f0a1a4ffc1b1dd6e5fb25
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-07-30 15:30:11 +0900

    繰越設定のフォームを作成した。

--- front/src/components/user/contentPage/PaymentOption.tsx
+++ front/src/components/user/contentPage/PaymentOption.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+
 interface BillingData {
     billingId: string;
     userId: string;
@@ -18,9 +20,49 @@ type Props = {
 
 export const PaymentOption = (props: Props) => {
     const billingData: BillingData = props.billingData;
-    return (
-        <div>
-            <h1>繰越設定</h1>
-        </div>
-    );
+    const carryOverType: string = props.carryOverType;
+
+    if (carryOverType == "no") {
+        return (
+            <div>
+                <p>繰越設定は行わず、全額を支払います。</p>
+                <Button
+                    size="small"
+                    variant="contained"
+                >
+                    変更を保存
+                </Button>
+            </div>
+        )
+    } else if (carryOverType == "part") {
+        return (
+            <div>
+                <p>一部を繰越し、今月の支払額を設定します。</p>
+                <Button
+                    size="small"
+                    variant="contained"
+                >
+                    変更を保存
+                </Button>
+            </div>
+        )
+    } else if (carryOverType == "all") {
+        return (
+            <div>
+                <p>全額を繰越し、今月は支払いを行いません。</p>
+                <Button
+                    size="small"
+                    variant="contained"
+                >
+                    変更を保存
+                </Button>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p>対応していない支払いオプションです。</p>
+            </div>
+        )
+    }
 }
