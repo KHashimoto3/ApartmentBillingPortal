9d1630fcf0d4c4894fe91bd62def740cca0de134
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-07 23:04:39 +0900

    dbへの接続設定を書いた。

--- back/database-test/main.go
+++ back/database-test/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 // 生徒のデータ形式
@@ -23,6 +30,28 @@ func helloName(c *gin.Context) {
 }
 
 func main() {
+	//環境設定ファイルの読み込み
+	err := godotenv.Load("config.env")
+	if err != nil {
+		log.Fatal("エラー: 環境設定ファイルを読み込めません！")
+	}
+
+	//設定情報を取る
+	userName := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", userName, password, host, port, dbName)
+
+	//データベース接続
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	router := gin.Default()
 	router.GET("/hello", hello)
 	router.GET("/hello-name", helloName)
