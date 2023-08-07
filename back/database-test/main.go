41e9cf84838b3dd256d3cb90cfec3a95024386ab
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-07 11:41:29 +0900

    クエリパラメータを受け取るhelloName APIを作成した。

--- back/database-test/main.go
+++ back/database-test/main.go
@@ -8,9 +8,16 @@ func hello(c *gin.Context) {
 	c.String(200, "hello, ワールド！")
 }
 
+func helloName(c *gin.Context) {
+	name := c.Query("name")
+	text := "hello, " + name
+	c.String(200, text)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/hello", hello)
+	router.GET("/hello-name", helloName)
 
 	router.Run(":8080")
 }
