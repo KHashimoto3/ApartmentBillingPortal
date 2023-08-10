e2c85dd785e93b4776c09149960cd8a8625b06fa
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-10 09:52:39 +0900

    apiからデータベースに接続できるようにした。

--- back/apart-bill-api/main.go
+++ back/apart-bill-api/main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
+
+var db *sql.DB
+var err error
 
 func hello(c *gin.Context) {
 	c.String(200, "hello!")
 }
 
 func main() {
+	//環境設定ファイルの読み込み
+	err = godotenv.Load("config.env")
+	if err != nil {
+		log.Fatal("エラー: 環境設定ファイルを読み込めません！")
+	}
+
+	//設定情報を取る
+	userName := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", userName, password, host, port, dbName)
+
+	//データベース接続
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	router := gin.Default()
 	router.GET("/", hello)
 
