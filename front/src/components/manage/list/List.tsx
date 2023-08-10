f06c0526a497d4ea43b89d0608edcb8d1f5b25c7
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-10 21:22:39 +0900

    請求の一覧と処理の画面を切り替えられるようにした。

--- front/src/components/manage/list/List.tsx
+++ front/src/components/manage/list/List.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+import { Select } from "./Select";
+import { Process } from "./Process";
+
 export const List = () => {
-    return (
-        <div className="historyContentsArea">
-            <h1>請求一覧</h1>
-        </div>
-    )
+    const [step, setStep] = useState<number>(0);
+
+    if (step === 0) {
+        return <Select />
+    } else if (step === 1) {
+        return <Process />
+    } else {
+        return(
+            <h1>ステップが無効です。</h1>
+        )
+    }
 }
