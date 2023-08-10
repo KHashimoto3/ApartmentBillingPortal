648d8109c6be9b2297b9bd3dc36fd22f9000914a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-10 22:24:38 +0900

    集金のチェック状況を表示するようにした。

--- front/src/components/manage/list/Select.tsx
+++ front/src/components/manage/list/Select.tsx
@@ -1,4 +1,7 @@
 import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
+import CheckIcon from '@mui/icons-material/Check';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 type Props = {
     step: number;
@@ -20,8 +23,9 @@ export const Select = (props: Props) => {
     const setUserId = props.setUserId;
 
     const listData: BillListData[] = [
-        {no: 0, userId: "test1", name: "山田 太郎", finalPrice: 3500, paid: 0},
-        {no: 1, userId: "test2", name: "鈴木太郎", finalPrice: 4000, paid: 1}
+        {no: 1, userId: "test1", name: "山田 太郎", finalPrice: 3500, paid: 0},
+        {no: 2, userId: "test2", name: "鈴木 太郎", finalPrice: 4000, paid: 1},
+        {no: 3, userId: "test3", name: "高橋 太郎", finalPrice: 3500, paid: 2},
     ];
 
     return (
@@ -42,13 +46,32 @@ export const Select = (props: Props) => {
                 onClick={() => {setUserId(data.userId); setStep(1);}}
                 key={data.no}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                style={{cursor: "pointer"}}
               >
                 <TableCell component="th" scope="row" align="center">
                   {data.no}
                 </TableCell>
                 <TableCell sx={{ minWidth: 150 }} align="center">{data.name}</TableCell>
                 <TableCell align="right">￥{data.finalPrice}</TableCell>
-                <TableCell align="right">チェック</TableCell>
+                {(() => {
+                if (data.paid === 0) {
+                    return (
+                        <TableCell align="right"><HorizontalRuleIcon /></TableCell>
+                    )
+                } else if (data.paid === 1) {
+                    return (
+                        <TableCell align="right"><CheckIcon /></TableCell>
+                    )
+                } else if (data.paid === 2) {
+                    return (
+                        <TableCell align="right"><ChangeCircleIcon /></TableCell>
+                    )
+                } else {
+                    return (
+                        <TableCell align="right"></TableCell>
+                    )
+                }
+            })()}
               </TableRow>
             ))}
           </TableBody>
