32a01eb97fb3f5a5b0741af87729f7b15cb616d3
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-12 09:02:57 +0900

    管理者画面のテーマをオレンジにした。

--- front/src/components/manage/ManageViewController.tsx
+++ front/src/components/manage/ManageViewController.tsx
@@ -10,13 +10,24 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 import { ManageViewContents } from './ManageViewContents';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 export const ManageViewController = () => {
     const [navigationValue, setNavigationValue] = useState<number>(0);
 
+    const manageTheme = createTheme({
+        palette: {
+            mode: 'light',
+            primary: {
+                main: '#fa7414'
+            }
+        }
+    })
+
     return (
         <div>
             <Container maxWidth="md" style={{height:"70vh"}}>
+                <ThemeProvider theme={manageTheme}>
                 <ManageViewContents contentsNo={navigationValue} />
                 <BottomNavigation
                     showLabels
@@ -29,6 +40,7 @@ export const ManageViewController = () => {
                     <BottomNavigationAction label="請求一覧・処理" icon={<FormatListBulletedIcon />} />
                     <BottomNavigationAction label="請求日設定" icon={<SettingsIcon />} />
                 </BottomNavigation>
+                </ThemeProvider>
             </Container>
         </div>
     );
