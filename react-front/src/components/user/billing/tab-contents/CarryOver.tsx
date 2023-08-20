4707fc0cf986d372acb64e8f81e77b3fbbfef918
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-08-20 14:52:30 +0900

    繰越タイプの選択と設定を別の画面にした。

--- react-front/src/components/user/billing/tab-contents/CarryOver.tsx
+++ react-front/src/components/user/billing/tab-contents/CarryOver.tsx
@@ -19,32 +19,56 @@ export const CarryOver = (props: Props) => {
   const billingData = props.billingData;
   const [value, setValue] = useState<string>('no');
 
-  return (
-    <Container
-      sx={{
-        width: '90%',
-        textAlign: 'center',
-        marginTop: '20px',
-        marginBottom: '50px',
-      }}
-    >
-      <Typography variant="h5">繰越のタイプを選択</Typography>
-      <FormControl>
-        <RadioGroup
-          row
-          aria-labelledby="demo-radio-buttons-group-label"
-          value={value}
-          name="radio-buttons-group"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setValue(e.target.value)
-          }
-        >
-          <FormControlLabel value="no" control={<Radio />} label="繰越なし" />
-          <FormControlLabel value="part" control={<Radio />} label="一部繰越" />
-          <FormControlLabel value="all" control={<Radio />} label="全額繰越" />
-        </RadioGroup>
+  const [selectPageIsShow, setSelectPageIsShow] = useState<boolean>(true);
+
+  if (selectPageIsShow) {
+    return (
+      <Container
+        sx={{
+          width: '90%',
+          textAlign: 'center',
+          marginTop: '20px',
+          marginBottom: '50px',
+        }}
+      >
+        <Typography variant="h5">繰越のタイプを選択</Typography>
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            value={value}
+            name="radio-buttons-group"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setValue(e.target.value)
+            }
+          >
+            <FormControlLabel value="no" control={<Radio />} label="繰越なし" />
+            <FormControlLabel
+              value="part"
+              control={<Radio />}
+              label="一部繰越"
+            />
+            <FormControlLabel
+              value="all"
+              control={<Radio />}
+              label="全額繰越"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Container>
+    );
+  } else {
+    return (
+      <Container
+        sx={{
+          width: '90%',
+          textAlign: 'center',
+          marginTop: '20px',
+          marginBottom: '50px',
+        }}
+      >
         <PaymentOption billingData={billingData} carryOverType={value} />
-      </FormControl>
-    </Container>
-  );
+      </Container>
+    );
+  }
 };
